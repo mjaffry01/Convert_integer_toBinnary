b7cee3c700b01839b1422e86e0848a48dd5eef5e
Md Ali Jaffry <mjaffry04@outlook.com>
2024-10-07 03:00:54 +0530

    Add project files.

---
+++ Convert_integer_toBinnary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Define an integer value to convert
+        int number = 25;
+
+        // Convert the integer to binary using Convert.ToString() method
+        string binaryRepresentation = Convert.ToString(number, 2);
+
+        // Print the result
+        Console.WriteLine($"The binary representation of {number} is: {binaryRepresentation}");
+
+        // Allow the user to enter a number
+        Console.WriteLine("\nEnter an integer to convert it to binary:");
+        int inputNumber = int.Parse(Console.ReadLine());
+
+        // Convert user input to binary and print
+        string binaryResult = Convert.ToString(inputNumber, 2);
+        Console.WriteLine($"The binary representation of {inputNumber} is: {binaryResult}");
+
+        Console.ReadLine();
+    }
+}
